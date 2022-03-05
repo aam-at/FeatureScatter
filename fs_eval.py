2d218415c6081b49b58695421ab64b36e2cbea43
Alexander Matyasko <alexander.matyasko@gmail.com>
2022-03-06 01:37:45 +0800

    Update eval script

--- fs_eval.py
+++ fs_eval.py
@@ -32,6 +32,7 @@ parser.add_argument('--resume',
                     action='store_true',
                     help='resume from checkpoint')
 parser.add_argument('--attack', default=True, type='bool', help='attack')
+parser.add_argument('--data_dir', type=str, help='path where to store the data')
 parser.add_argument('--model_dir', type=str, help='model path')
 parser.add_argument('--init_model_pass',
                     default='-1',
@@ -58,20 +59,28 @@ parser.add_argument('--image_size', default=32, type=int, help='image size')
 
 args = parser.parse_args()
 
+
+def print_log(print_string, log):
+    print("{}".format(print_string))
+    log.write('{}\n'.format(print_string))
+    log.flush()
+
+log = open(os.path.join(args.model_dir, 'log.txt'), 'w')
+
 if args.dataset == 'cifar10':
-    print('------------cifar10---------')
+    print_log('------------cifar10---------', log)
     args.num_classes = 10
     args.image_size = 32
 elif args.dataset == 'cifar100':
-    print('----------cifar100---------')
+    print_log('----------cifar100---------', log)
     args.num_classes = 100
     args.image_size = 32
 if args.dataset == 'svhn':
-    print('------------svhn10---------')
+    print_log('------------svhn10---------', log)
     args.num_classes = 10
     args.image_size = 32
 elif args.dataset == 'mnist':
-    print('----------mnist---------')
+    print_log('----------mnist---------', log)
     args.num_classes = 10
     args.image_size = 28
 
@@ -79,32 +88,27 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 start_epoch = 0
 
 # Data
-print('==> Preparing data..')
+print_log('==> Preparing data..', log)
 
-if args.dataset == 'cifar10' or args.dataset == 'cifar100':
+if args.dataset == 'cifar10' or args.dataset == 'cifar100' or args.dataset == 'svhn':
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # [-1 1]
-    ])
-elif args.dataset == 'svhn':
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),  # [-1 1]
+        transforms.Lambda(lambda x: x.mul(255))
     ])
 
 if args.dataset == 'cifar10':
-    testset = torchvision.datasets.CIFAR10(root='./data',
+    testset = torchvision.datasets.CIFAR10(root=args.data_dir,
                                            train=False,
                                            download=True,
                                            transform=transform_test)
 elif args.dataset == 'cifar100':
-    testset = torchvision.datasets.CIFAR100(root='./data',
+    testset = torchvision.datasets.CIFAR100(root=args.data_dir,
                                             train=False,
                                             download=True,
                                             transform=transform_test)
 
 elif args.dataset == 'svhn':
-    testset = torchvision.datasets.SVHN(root='./data',
+    testset = torchvision.datasets.SVHN(root=args.data_dir,
                                         split='test',
                                         download=True,
                                         transform=transform_test)
@@ -114,49 +118,47 @@ testloader = torch.utils.data.DataLoader(testset,
                                          shuffle=False,
                                          num_workers=2)
 
-print('==> Building model..')
+print_log('==> Building model..', log)
 if args.dataset == 'cifar10' or args.dataset == 'cifar100' or args.dataset == 'svhn':
-    print('---wide resenet-----')
-    basic_net = WideResNet(depth=28,
-                           num_classes=args.num_classes,
-                           widen_factor=10)
+    print_log(args.model, log)
+    if args.model == 'wrn28_10':
+        net = wrn28_10(num_classes=args.num_classes, widen_factor=10, per_img_std=True)
+    elif args.model == 'preactresnet18':
+        net = preactresnet18(num_classes=args.num_classes, per_img_std=True)
 
-basic_net = basic_net.to(device)
+net = net.to(device)
 
 # configs
 config_natural = {'train': False}
 
 config_fgsm = {
-    'train': False,
     'targeted': False,
-    'epsilon': 8.0 / 255 * 2,
+    'epsilon': 8.0,
     'num_steps': 1,
-    'step_size': 8.0 / 255 * 2,
-    'random_start': True
+    'step_size': 8.0,
+    'random_start': False
 }
 
 config_pgd = {
-    'train': False,
     'targeted': False,
-    'epsilon': 8.0 / 255 * 2,
+    'epsilon': 8.0,
     'num_steps': 20,
-    'step_size': 2.0 / 255 * 2,
-    'random_start': True,
+    'step_size': 2.0,
+    'random_start': False,
     'loss_func': torch.nn.CrossEntropyLoss(reduction='none')
 }
 
 config_cw = {
-    'train': False,
     'targeted': False,
-    'epsilon': 8.0 / 255 * 2,
+    'epsilon': 8.0,
     'num_steps': 20,
-    'step_size': 2.0 / 255 * 2,
-    'random_start': True,
+    'step_size': 2.0,
+    'random_start': False,
     'loss_func': CWLoss(args.num_classes)
 }
 
 
-def test(epoch, net):
+def test(epoch, net, attack):
     net.eval()
     test_loss = 0
     correct = 0
@@ -167,16 +169,16 @@ def test(epoch, net):
         start_time = time.time()
         inputs, targets = inputs.to(device), targets.to(device)
 
-        pert_inputs = inputs.detach()
+        pert_inputs = attack(inputs, targets).detach()
 
-        outputs, _ = net(pert_inputs, targets, batch_idx=batch_idx)
+        pert_outputs, _ = net(pert_inputs, targets)
 
-        loss = criterion(outputs, targets)
+        loss = criterion(pert_outputs, targets)
         test_loss += loss.item()
 
         duration = time.time() - start_time
 
-        _, predicted = outputs.max(1)
+        _, predicted = pert_outputs.max(1)
         batch_size = targets.size(0)
         total += batch_size
         correct_num = predicted.eq(targets).sum().item()
@@ -185,13 +187,13 @@ def test(epoch, net):
             str(predicted.eq(targets).sum().item() / targets.size(0)))
 
         if batch_idx % args.log_step == 0:
-            print(
+            print_log(
                 "step %d, duration %.2f, test  acc %.2f, avg-acc %.2f, loss %.2f"
                 % (batch_idx, duration, 100. * correct_num / batch_size,
-                   100. * correct / total, test_loss / total))
+                   100. * correct / total, test_loss / total), log)
 
     acc = 100. * correct / total
-    print('Val acc:', acc)
+    print_log('Val acc:', acc, log)
     return acc
 
 
@@ -200,21 +202,23 @@ attack_num = len(attack_list)
 
 for attack_idx in range(attack_num):
 
-    args.attack_method = attack_list[attack_idx]
+    args.attack_method = attack_list[attack_idx].upper()
 
     if args.attack_method == 'natural':
-        print('-----natural non-adv mode -----')
+        print_log('-----natural non-adv mode -----', log)
         # config is only dummy, not actually used
-        net = Attack_None(basic_net, config_natural)
-    elif args.attack_method.upper() == 'FGSM':
-        print('-----FGSM adv mode -----')
-        net = Attack_PGD(basic_net, config_fgsm)
-    elif args.attack_method.upper() == 'PGD':
-        print('-----PGD adv mode -----')
-        net = Attack_PGD(basic_net, config_pgd)
-    elif args.attack_method.upper() == 'CW':
-        print('-----CW adv mode -----')
-        net = Attack_PGD(basic_net, config_cw)
+        attack = Attack_None(net, config_natural)
+    elif args.attack_method == 'FGSM':
+        print_log('-----FGSM adv mode -----', log)
+        attack = Attack_PGD(net, config_fgsm)
+    elif args.attack_method.startswith('PGD'):
+        print_log('-----PGD adv mode -----', log)
+        num_steps = int(args.attack_method.replace('PGD', ''))
+        attack = Attack_PGD(net, config_pgd.update({'num_steps': num_steps}))
+    elif args.attack_method.startswith('CW'):
+        print_log('-----CW adv mode -----', log)
+        num_steps = int(args.attack_method.replace('CW', ''))
+        attack = Attack_PGD(net, config_cw.update({'num_steps': num_steps}))
     else:
         raise Exception(
             'Should be a valid attack method. The specified attack method is: {}'
@@ -226,25 +230,25 @@ for attack_idx in range(attack_num):
 
     if args.resume and args.init_model_pass != '-1':
         # Load checkpoint.
-        print('==> Resuming from checkpoint..')
+        print_log('==> Resuming from checkpoint..', log)
         f_path_latest = os.path.join(args.model_dir, 'latest')
         f_path = os.path.join(args.model_dir,
                               ('checkpoint-%s' % args.init_model_pass))
         if not os.path.isdir(args.model_dir):
-            print('train from scratch: no checkpoint directory or file found')
+            print_log('train from scratch: no checkpoint directory or file found', log)
         elif args.init_model_pass == 'latest' and os.path.isfile(
                 f_path_latest):
             checkpoint = torch.load(f_path_latest)
             net.load_state_dict(checkpoint['net'])
             start_epoch = checkpoint['epoch']
-            print('resuming from epoch %s in latest' % start_epoch)
+            print_log('resuming from epoch %s in latest' % start_epoch, log)
         elif os.path.isfile(f_path):
             checkpoint = torch.load(f_path)
             net.load_state_dict(checkpoint['net'])
             start_epoch = checkpoint['epoch']
-            print('resuming from epoch %s' % start_epoch)
+            print_log('resuming from epoch %s' % start_epoch, log)
         elif not os.path.isfile(f_path) or not os.path.isfile(f_path_latest):
-            print('train from scratch: no checkpoint directory or file found')
+            print_log('train from scratch: no checkpoint directory or file found', log)
 
     criterion = nn.CrossEntropyLoss()
 
